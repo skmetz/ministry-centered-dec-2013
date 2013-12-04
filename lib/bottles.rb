8323538a8d36e879b84bd3b8f742ae5289af02b1
Katrina Owen <katrina.owen@gmail.com>
2013-12-04 11:39:58 -0800

    DRY out the bottles

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -3,20 +3,60 @@ class BottlesSong
     verses(99, 0)
   end
 
-  def verses(upper_bound, lower_bound)
-    upper_bound.downto(lower_bound).collect {|n| verse(n) + "\n"}.join
+  def verses(starting, ending)
+    starting.downto(ending).map {|n| verse(n)}.join("\n") + "\n"
   end
 
   def verse(number)
-    case number
-    when 0
-      "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
-    when 1
-      "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
-    when 2
-      "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+    "#{count(number).to_s.capitalize} #{container(number)} of #{liquid} #{location}, " +
+    "#{count(number)} #{container(number)} of #{liquid}.\n" +
+    "#{action(number)}, " +
+    "#{count(number-1)} #{container(number-1)} of #{liquid} #{location}.\n"
+  end
+
+  private
+  def action(number)
+    if number == 0
+      "Go to the store and buy some more"
     else
-      "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number-1} bottles of beer on the wall.\n"
+      "Take #{pronoun(number)} down and pass it around"
     end
   end
+
+  def count(number)
+    case number
+    when -1
+      99
+    when 0
+      'no more'
+    else
+      number
+    end
+  end
+
+  def container(number)
+    if number == 1
+      'bottle'
+    else
+      'bottles'
+    end
+  end
+
+  def pronoun(number)
+    if number == 1
+      'it'
+    else
+      'one'
+    end
+  end
+
+
+  def liquid
+    'beer'
+  end
+
+  def location
+    'on the wall'
+  end
+
 end
